8527546dfc2a4f80982722a462d2a4731de5456d
KdRome <r.petliak12@gmail.com>
2023-11-26 19:58:18 -0500

    Added Order of Execution

--- algorithms.cpp
+++ algorithms.cpp
@@ -8,6 +8,8 @@ void firstComeFirstServe(vector<Process>& processes) {
         });
 
     int currentTime = 0;
+    vector<string> executionOrder; // Vector to store the order of execution
+
     for (auto& process : processes) {
         if (currentTime < process.arrivalTime) {
             currentTime = process.arrivalTime;
@@ -16,13 +18,22 @@ void firstComeFirstServe(vector<Process>& processes) {
         process.completionTime = process.startTime + process.burstTime;
         process.turnaroundTime = process.completionTime - process.arrivalTime;
         currentTime = process.completionTime;
+        executionOrder.push_back(process.name); // Add to execution order
     }
+
+    // Print the order of execution
+    cout << "Order of execution: ";
+    for (const auto& processName : executionOrder) {
+        cout << processName << " -> ";
+    }
+    cout << "end" << endl;
 }
 
 void priority(vector<Process>& processes) {
     int currentTime = 0;
     int completed = 0;
     int n = processes.size();
+    vector<string> executionOrder; // Vector to store the order of execution
 
     while (completed != n) {
         int idx = -1;
@@ -41,7 +52,6 @@ void priority(vector<Process>& processes) {
                 }
             }
         }
-
         if (idx != -1) {
             if (processes[idx].startTime == -1) {
                 processes[idx].startTime = currentTime;
@@ -53,18 +63,28 @@ void priority(vector<Process>& processes) {
                 processes[idx].completionTime = currentTime;
                 processes[idx].turnaroundTime = processes[idx].completionTime - processes[idx].arrivalTime;
                 completed++;
+                executionOrder.push_back(processes[idx].name); // Add to execution order upon completion
             }
         }
         else {
             currentTime++;
         }
     }
+
+    // Print the order of execution
+    cout << "Order of execution: ";
+    for (const auto& processName : executionOrder) {
+        cout << processName << " -> ";
+    }
+    cout << "end" << endl;
 }
+
 
 void shortestJobNext(vector<Process>& processes) {
     int currentTime = 0;
     int completed = 0;
     int n = processes.size();
+    vector<string> executionOrder; // Vector to store the order of execution
 
     while (completed != n) {
         int idx = -1;
@@ -76,25 +96,33 @@ void shortestJobNext(vector<Process>& processes) {
                 idx = i;
             }
         }
-
         if (idx != -1) {
             processes[idx].startTime = currentTime;
             processes[idx].completionTime = currentTime + processes[idx].burstTime;
             processes[idx].turnaroundTime = processes[idx].completionTime - processes[idx].arrivalTime;
             currentTime = processes[idx].completionTime;
             completed++;
+            executionOrder.push_back(processes[idx].name); // Add to execution order upon completion
         }
         else {
             currentTime++;
         }
     }
+    // Print the order of execution
+    cout << "Order of execution: ";
+    for (const auto& processName : executionOrder) {
+        cout << processName << " -> ";
+    }
+    cout << "end" << endl;
 }
 
+
 void roundRobin(vector<Process>& processes, int quantum) {
-    
     int currentTime = processes.empty() ? 0 : processes[0].arrivalTime;
     queue<int> processQueue;
+    vector<string> executionOrder; // Vector to store the order of execution
 
+    // Enqueue processes based on arrival time
     for (int i = 0; i < processes.size(); ++i) {
         if (processes[i].arrivalTime <= currentTime) {
             processQueue.push(i);
@@ -105,11 +133,16 @@ void roundRobin(vector<Process>& processes, int quantum) {
         int idx = processQueue.front();
         processQueue.pop();
 
-        if (processes[idx].remainingBurstTime <= 0) continue; // Skip already completed processes
+        // Skip already completed processes
+        if (processes[idx].remainingBurstTime <= 0) continue;
 
         if (processes[idx].startTime == -1) {
             processes[idx].startTime = currentTime;
         }
+
+        // Record the execution order as the process gets the CPU
+        executionOrder.push_back(processes[idx].name);
+
         int executionTime = min(quantum, processes[idx].remainingBurstTime);
         processes[idx].remainingBurstTime -= executionTime;
         currentTime += executionTime;
@@ -120,12 +153,22 @@ void roundRobin(vector<Process>& processes, int quantum) {
                 processQueue.push(i);
             }
         }
+
+        // Re-enqueue if not finished
         if (processes[idx].remainingBurstTime > 0) {
-            processQueue.push(idx); // Re-enqueue if not finished
+            processQueue.push(idx);
         }
         else {
-            processes[idx].completionTime = currentTime; // Set completion time
+            // Set completion time
+            processes[idx].completionTime = currentTime;
             processes[idx].turnaroundTime = processes[idx].completionTime - processes[idx].arrivalTime;
         }
     }
+
+    // Print the order of execution
+    cout << "Order of execution: ";
+    for (const auto& processName : executionOrder) {
+        cout << processName << " -> ";
+    }
+    cout << "end" << endl;
 }
