7741bc2c4d409f3a954a37d7841b4b8a4f0a6324
KdRome <r.petliak12@gmail.com>
2023-11-28 11:37:55 -0500

    Test

--- algorithms.h
+++ algorithms.h
@@ -1,7 +1,6 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 #include "process.h"
-
 void firstComeFirstServe(vector<Process>& processes);
 void priority(vector<Process>& processes);
 void shortestJobNext(vector<Process>& processes);
